59788d47fe80bd781acbc96c310a0f9347085f75
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-16 22:39:01 +0530

    complete all data types

---
+++ Basics/basics.js
@@ -0,0 +1,26 @@
+let a = 10;
+console.log(typeof(a));
+
+let b = 23.45678
+console.log(typeof(b));
+
+let c = "Hello"
+console.log(typeof(c));
+
+let d = true
+console.log(typeof(d));
+
+let e = null
+console.log(typeof(e));
+
+let f = undefined
+console.log(typeof(f));
+
+let g = 12n
+console.log(typeof(g));
+
+let g2 = 123456789876543212345678765432123456765432234n
+console.log(typeof(g2));
+
+let h = Symbol("symbol");
+console.log(typeof(h));
---
+++ Basics/guide.md
@@ -0,0 +1,9 @@
+1. Variables: var, let, const
+
+2. Data Types: String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+3. Type Conversion & Coercion
+
+4. Operators: Arithmetic, Assignment, Comparison, Logical, Bitwise, Ternary
+
+5. Template Literals
