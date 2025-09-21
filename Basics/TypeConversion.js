4e89833f1590c230315fa7c79078a77e494e6631
bhavy1204 <bhavysarwa51@gmail.com>
2025-09-21 21:30:40 +0530

    add type coversiona nd data types

---
+++ Basics/TypeConversion.js
@@ -0,0 +1,13 @@
+// Type conversions
+
+let a = 123;
+
+console.log(typeof(a));
+a = String(a);
+console.log(typeof(a));
+
+let a1 =234;
+
+console.log(typeof(a1));
+a1 = a.toString();
+console.log(typeof(a1));
